2171c517c9373ba3dd9eb9fe348ad1279e21953d
nagar <nagar@192.168.1.37>
2025-08-09 01:03:39 +0530

    Task 1 by Dev1

--- GitProj1/src/main/java/com/nit/main/App.java
+++ GitProj1/src/main/java/com/nit/main/App.java
@@ -1,10 +1,19 @@
 package com.nit.main;
 
-/**
- * Hello world!
- */
+
 public class App {
+	
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      
+    	App a1=new App();
+        int result=a1.sum(10, 20);
+        
+        System.out.println("addition is :"+result);
+    	
     }
 }
--- GitProj1/src/test/java/com/nit/main/AppTest.java
+++ GitProj1/src/test/java/com/nit/main/AppTest.java
@@ -1,5 +1,6 @@
 package com.nit.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,32 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void additionOfTwoNumbers() {
+    	
+    	App a1=new App();
+    	
+    	int excepted=30;
+    	
+    	int actual=a1.sum(10, 20);
+    	
+    	
+    	
+        assertEquals(excepted,actual);
+    }
+    
+    @Test
+    public void substractionOfTwoNumbers() {
+    	
+    	App a1=new App();
+    	
+    	int excepted=-30;
+    	
+    	int actual=a1.sum(-10, -20);
+    	
+    	
+    	
+        assertEquals(excepted,actual);
     }
 }
