a23cc21f95c377f628ee00b2fa47bef2a2460d53
nagar <nagar@192.168.1.50>
2025-08-10 00:18:50 +0530

    Dev1 Commit

--- GitProj1/src/test/java/com/nit/main/AppTest.java
+++ GitProj1/src/test/java/com/nit/main/AppTest.java
@@ -1,7 +1,6 @@
 package com.nit.main;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
